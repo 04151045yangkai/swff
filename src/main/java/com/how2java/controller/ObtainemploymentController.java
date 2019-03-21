445b33548df86e671f36ee53251e2db4823b649b
cytcool <675149948@qq.com>
2019-03-21 16:27:17 +0800

    就业意向信息

---
+++ src/main/java/com/how2java/controller/ObtainemploymentController.java
@@ -0,0 +1,25 @@
+package com.how2java.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.how2java.pojo.Obtainemployment;
+import com.how2java.service.ObtainemploymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ObtainemploymentController {
+
+    @Autowired
+    ObtainemploymentService oeService;
+
+    @ResponseBody
+    @RequestMapping(value = "loadPoorCardData" ,method = RequestMethod.POST)
+    public String loadDataByID( @RequestParam("oeid") Integer oeid) {
+        Obtainemployment obtainemployment = oeService.checkDataByID(oeid);
+        return  JSONObject.toJSON(obtainemployment).toString();
+    }
+}
